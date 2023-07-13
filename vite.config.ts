8a01ce8f966fdc53b577c7369330b73bad1fa60a
Eric Le Ponner <eric.leponner@icloud.com>
2023-07-13 19:51:53 +0200

    Added vite-plugin-node-polyfills to polyfill Buffer required by hashgraph/proto.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    nodePolyfills({ // hashgraph/proto needs NodeJS buffers
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      globals: {
+        Buffer: true,
+        global: false,
+        process: false,
+      },
+      protocolImports: false,
+    }),
   ],
   resolve: {
     alias: {
